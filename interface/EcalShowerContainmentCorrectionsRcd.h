13db27298119a72e7164d465456fbb27c2b1887d
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2007-05-15 20:55:18 +0000

    First commit

---
+++ interface/EcalShowerContainmentCorrectionsRcd.h
@@ -0,0 +1,26 @@
+#ifndef CondFormats_EcalShowerContainmentCorrectionsRcd_h
+#define CondFormats_EcalShowerContainmentCorrectionsRcd_h
+// -*- C++ -*-
+//
+// Package:     CondFormats
+// Class  :     EcalShowerContainmentCorrectionsRcd
+// 
+/**\class EcalShowerContainmentCorrectionsRcd EcalShowerContainmentCorrectionsRcd.h src/CondFormats/interface/EcalShowerContainmentCorrectionsRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Fri Mar  2 16:50:49 CET 2007
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class EcalShowerContainmentCorrectionsRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalShowerContainmentCorrectionsRcd> {};
+
+#endif
---
+++ src/EcalShowerContainmentCorrectionsRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     CondFormats
+// Class  :     EcalShowerContainmentCorrectionRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Fri Mar  2 16:50:49 CET 2007
+// $Id$
+
+#include "CondFormats/DataRecord/interface/EcalShowerContainmentCorrectionsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(EcalShowerContainmentCorrectionsRcd);
