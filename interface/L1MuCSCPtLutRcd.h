94c24135ee78c7b33840832b4cbdb994b41dc4af
cmsbuild <>
2007-06-13 12:39:27 +0000

    This commit was manufactured by cvs2git to create tag 'CondFormats- DataRecord-SiStrip_x140_V0'.
    Sprout from master 2007-06-13 12:39:26 UTC Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch> 'first commit'
    Delete:
        interface/BTauGenericMVAJetTagComputerRcd.h
        interface/DTConfigManagerRcd.h
        interface/L1CSCTPParametersRcd.h
        interface/L1MuCSCDTLutRcd.h
        interface/L1MuCSCGlobalLutsRcd.h
        interface/L1MuCSCLocalPhiLutRcd.h
        interface/L1MuCSCPtLutRcd.h
        src/BTauGenericMVAJetTagComputerRcd.cc
        src/DTConfigManagerRcd.cc
        src/L1CSCTPParametersRcd.cc
        src/L1MuCSCDTLutRcd.cc
        src/L1MuCSCGlobalLutsRcd.cc
        src/L1MuCSCLocalPhiLutRcd.cc
        src/L1MuCSCPtLutRcd.cc

--- interface/L1MuCSCPtLutRcd.h
+++
@@ -1,8 +0,0 @@
-#ifndef CondFormatsDataRecord_L1MuCSCPtLutRcd_h
-#define CondFormatsDataRecord_L1MuCSCPtLutRcd_h
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-
-class L1MuCSCPtLutRcd : public edm::eventsetup::EventSetupRecordImplementation<L1MuCSCPtLutRcd> {};
-
-#endif
